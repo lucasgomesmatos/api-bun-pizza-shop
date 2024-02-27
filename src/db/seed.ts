6e2a314fb8aeef6892ad3564c779ae21f676fe37
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-02-26 23:06:20 -0300

    feat: teste

---
+++ src/db/seed.ts
@@ -0,0 +1,67 @@
+import { faker } from '@faker-js/faker';
+import chalk from 'chalk';
+import { db } from './connection';
+import { restaurants, users } from "./schema";
+
+/**
+ * Reset database
+ */
+
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellow("✅ Database reset successfully"));
+
+/**
+ * create customer
+ */
+
+await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: "customer",
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: "customer",
+  }
+])
+
+console.log(chalk.yellow("✅ Users customer created successfully"));
+
+
+/**
+ * create manager
+ */
+
+const [manager] = await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: "admin@admin.com",
+    role: "manager",
+  },
+
+]).returning({
+  id: users.id,
+});
+
+console.log(chalk.yellow("✅ Users manager created successfully"));
+
+/**
+ * create restaurant
+ */
+
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id,
+  }
+])
+
+console.log(chalk.yellow("✅ Restaurant created successfully"));
+
+process.exit();
+
