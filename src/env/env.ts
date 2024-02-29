13ef59fb5f04a50abf82f20f1f3c2504afb22cb3
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-02-28 21:07:21 -0300

    feat: jwt and cookie

--- src/env/env.ts
+++ src/env/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url().min(1),
   API_BASE_URL: z.string().url().min(1),
   AUTH_REDIRECT_URL: z.string().url().min(1),
+  SECRET_KEY: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,33 @@
-import { Elysia } from 'elysia'
+import cookie from '@elysiajs/cookie'
+import { cors } from '@elysiajs/cors'
+import jwt from '@elysiajs/jwt'
+import swagger from '@elysiajs/swagger'
+import { Elysia, t } from 'elysia'
 import { env } from '../env/env'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+      algorithms: ['HS256'],
+    }),
+  )
+  .use(cookie())
+  .use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    }),
+  )
+  .use(swagger())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(env.PORT, () => {
   console.log('Server is running on port 3333')
